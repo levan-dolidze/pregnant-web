0ef353db4c7738eef43069ae07ff6fbc40a84bf8
levan-dolidze <64006772+levan-dolidze@users.noreply.github.com>
2026-06-11 16:42:04 +0400

    up

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../../components/button/button.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
