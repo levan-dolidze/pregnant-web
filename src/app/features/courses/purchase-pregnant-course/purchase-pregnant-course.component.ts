ee6a0b12d1cf969c6582b93aab233df4144d5f9a
levan-dolidze <64006772+levan-dolidze@users.noreply.github.com>
2026-06-14 22:25:25 +0400

    up

--- src/app/features/courses/purchase-pregnant-course/purchase-pregnant-course.component.ts
+++ src/app/features/courses/purchase-pregnant-course/purchase-pregnant-course.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { InputComponent } from 'src/app/components/input/input.component';
+import { ButtonComponent } from 'src/app/components/button/button.component';
+import { ValidationErrorsDirective } from 'src/app/shared/directives/validation-errors.directive';
+import { ControlModeChange } from 'src/app/shared/functions/controlModeChange';
 
 @Component({
   selector: 'app-purchase-pregnant-course',
-  imports: [],
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    InputComponent,
+    ButtonComponent,
+    ValidationErrorsDirective,
+  ],
   templateUrl: './purchase-pregnant-course.component.html',
-  styleUrl: './purchase-pregnant-course.component.scss'
+  styleUrl: './purchase-pregnant-course.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PurchasePregnantCourseComponent {
+  readonly form = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    surname: new FormControl('', [Validators.required]),
+    mob: new FormControl('', [Validators.required, Validators.pattern(/^\d+$/)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+  });
 
+  onPay(): void {
+    if (this.form.invalid) {
+      ControlModeChange.formFieldsModeControl('markAsDirty', this.form);
+
+    }
+  }
 }
