27e9806b0151a9d177bb15faa20125e41e3cddbe
levan-dolidze <64006772+levan-dolidze@users.noreply.github.com>
2026-07-23 21:05:38 +0400

    intro up

--- src/app/components/sidebar/sidebar-data.ts
+++ src/app/components/sidebar/sidebar-data.ts
@@ -4,15 +4,15 @@ export const navItems: NavItem[] = [
   {
     displayName: 'შესავალი',
     children: [
-      { displayName: 'ვინ ვარ', route: '/courses/test/1' },
-      { displayName: 'რას შეისწავლი', route: '/courses/test/2' },
+      { displayName: 'ვინ ვარ', route: '/courses/into/1' },
+      { displayName: 'რას შეისწავლი', route: '/courses/into/2' },
     ],
   },
   {
     displayName: 'ბავშვის კვება',
     children: [
-      { displayName: 'ძუძუთი კვება', route: '/courses/child-nutrition/breastfeeding' },
-      { displayName: 'ბოთლით კვება', route: '/courses/child-nutrition/bottle-feeding' },
+      { displayName: 'ძუძუთი კვება', route: '/courses/child-nutrition/3' },
+      { displayName: 'ბოთლით კვება', route: '/courses/child-nutrition/4' },
     ],
   },
 ];
