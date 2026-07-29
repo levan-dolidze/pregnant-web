46015cbc5761052f1b0892d8d7751353eb6c01f9
levan-dolidze <64006772+levan-dolidze@users.noreply.github.com>
2026-07-29 18:08:59 +0400

    update  step

--- src/app/features/courses-promo/purchase-course/contact-info/contact-info.component.ts
+++ src/app/features/courses-promo/purchase-course/contact-info/contact-info.component.ts
@@ -1,15 +1,16 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { InputComponent } from 'src/app/components/input/input.component';
 import { ButtonComponent } from 'src/app/components/button/button.component';
-import { ValidationErrorsDirective } from 'src/app/shared/directives/validation-errors.directive';
 import { OtpComponent } from 'src/app/shared/features/otp/otp.component';
+import { ValidationErrorsDirective } from 'src/app/shared/directives/validation-errors.directive';
 import { ControlModeChange } from 'src/app/shared/functions/controlModeChange';
+import { regExp } from 'src/app/shared/utils/regex';
 
 @Component({
   selector: 'app-contact-info',
-  imports: [ReactiveFormsModule, MatFormFieldModule, InputComponent, ButtonComponent, ValidationErrorsDirective, OtpComponent],
+  imports: [ReactiveFormsModule, MatFormFieldModule, InputComponent, ButtonComponent, OtpComponent, ValidationErrorsDirective],
   templateUrl: './contact-info.component.html',
   styleUrl: './contact-info.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -17,9 +18,10 @@ import { ControlModeChange } from 'src/app/shared/functions/controlModeChange';
 export class ContactInfoComponent {
 
   form = new FormGroup({
-    mobileNumber: new FormControl(),
-    email: new FormControl()
+    email: new FormControl('', [Validators.required, Validators.pattern(regExp.email)]),
+    mobileNumber: new FormControl('', [Validators.required, Validators.pattern(regExp.mobileGe)])
   })
+  
   @Output() confirmOtpEmit = new EventEmitter<boolean>();
 
   onNext(): void {
--- src/app/features/courses-promo/purchase-course/personal-info/personal-info.component.ts
+++ src/app/features/courses-promo/purchase-course/personal-info/personal-info.component.ts
@@ -1,12 +1,14 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { InputComponent } from 'src/app/components/input/input.component';
 import { ButtonComponent } from 'src/app/components/button/button.component';
+import { ValidationErrorsDirective } from 'src/app/shared/directives/validation-errors.directive';
 import { ControlModeChange } from 'src/app/shared/functions/controlModeChange';
 
 @Component({
   selector: 'app-personal-info',
-  imports: [ReactiveFormsModule, InputComponent, ButtonComponent],
+  imports: [ReactiveFormsModule, MatFormFieldModule, InputComponent, ButtonComponent, ValidationErrorsDirective],
   templateUrl: './personal-info.component.html',
   styleUrl: './personal-info.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -14,19 +16,19 @@ import { ControlModeChange } from 'src/app/shared/functions/controlModeChange';
 export class PersonalInfoComponent {
 
   form = new FormGroup({
-    userName: new FormControl(),
-    userLastName: new FormControl(),
+    userName: new FormControl('', [Validators.required]),
+    userLastName: new FormControl('', [Validators.required]),
   })
   @Output() next = new EventEmitter<void>();
 
   onNext(): void {
 
-   if(this.form.invalid) {
-     ControlModeChange.formFieldsModeControl('markAsDirty', this.form);
-   }
-   else{
+    if (this.form.invalid) {
+      ControlModeChange.formFieldsModeControl('markAsDirty', this.form);
+    }
+    else {
 
-   }
+    }
 
   }
 }
